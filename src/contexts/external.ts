31c01ee24696193519358c0506d7e267e951c574
Jakub Stefanisin <kouby98@gmail.com>
2022-04-22 11:32:33 +0200

    fix: type fix

--- src/contexts/external.ts
+++ src/contexts/external.ts
@@ -1,7 +1,14 @@
 import { createContext } from 'react';
+import Animated from 'react-native-reanimated';
+import { SHEET_STATE } from '../constants';
 import type { BottomSheetMethods, BottomSheetVariables } from '../types';
 
-export const BottomSheetContext =
-  createContext<(BottomSheetMethods & BottomSheetVariables) | null>(null);
+export const BottomSheetContext = createContext<
+  | (BottomSheetMethods &
+      BottomSheetVariables & {
+        animatedSheetState: Animated.SharedValue<SHEET_STATE>;
+      })
+  | null
+>(null);
 
 export const BottomSheetProvider = BottomSheetContext.Provider;
